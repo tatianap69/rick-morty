4afce92b1351514b9640c0371875a2285ec836ca
tatianap69 <angieepaez69@gmail.com>
2025-06-07 06:22:31 -0500

    Añadir las rutas básicas

--- rick-morty/src/app/app.routes.ts
+++ rick-morty/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { PersonajesLista } from './componentes/personajes-lista/personajes-lista';
+import { PersonajesDetalles } from './componentes/personajes-detalles/personajes-detalles';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', component: PersonajesLista },
+    { path: 'personaje/:name', component: PersonajesDetalles },
+    { path: '**', component: PersonajesLista },
+];
