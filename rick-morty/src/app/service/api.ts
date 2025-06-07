f8564b0cfc1b4c3a63b143bd0bce9a0a13d4140a
tatianap69 <angieepaez69@gmail.com>
2025-06-07 06:45:21 -0500

    Añadir métodos principales

--- rick-morty/src/app/service/api.ts
+++ rick-morty/src/app/service/api.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class Api {
+  baseUrl: string = 'https://rickandmortyapi.com/api/character';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getPersonajes(limit: number = 20, page: number = 1): Observable<any> {
+    return this.http.get(`${this.baseUrl}?limit=${limit}&page=${page}`);
+  }
+
+  getDetallePersonaje(id: number): Observable<any> {
+    return this.http.get(`${this.baseUrl}/${id}`);
+  }
 }
