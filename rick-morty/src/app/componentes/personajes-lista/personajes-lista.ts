49fab46b62c0031320ce6a3b97d128477e548ec4
tatianap69 <angieepaez69@gmail.com>
2025-06-07 21:11:07 -0500

    Mostrar todos los personajes

--- rick-morty/src/app/componentes/personajes-lista/personajes-lista.ts
+++ rick-morty/src/app/componentes/personajes-lista/personajes-lista.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+// personajes-lista.ts
+import { Component, OnInit } from '@angular/core';
+import { Api } from '../../service/api';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-personajes-lista',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './personajes-lista.html',
-  styleUrl: './personajes-lista.scss'
+  styleUrl: './personajes-lista.scss',
 })
-export class PersonajesLista {
+export class PersonajesLista implements OnInit {
+  personajes: any[] = [];
 
+  constructor(private api: Api) {}
+
+  ngOnInit() {
+    this.api.personajes$.subscribe((data) => {
+      this.personajes = data;
+    });
+    
+    this.api.cargarInicial();
+  }
 }
