2d8dc9c1197ca88643aa05af98eaf138e8058b52
tatianap69 <angieepaez69@gmail.com>
2025-06-07 21:10:30 -0500

    Mostrar el detalle de los personajes

--- rick-morty/src/app/componentes/personajes-detalles/personajes-detalles.ts
+++ rick-morty/src/app/componentes/personajes-detalles/personajes-detalles.ts
@@ -1,11 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Api } from '../../service/api';
+import { ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-personajes-detalles',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './personajes-detalles.html',
-  styleUrl: './personajes-detalles.scss'
+  styleUrl: './personajes-detalles.scss',
 })
-export class PersonajesDetalles {
+export class PersonajesDetalles implements OnInit {
+  personajeDetalle: any;
 
+  constructor(private api: Api, private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.route.paramMap.subscribe((params) => {
+      const id = Number(params.get('id'));
+      if (id) {
+        this.api.getDetallePersonaje(id).subscribe({
+          next: (data: any) => {
+            this.personajeDetalle = data;
+          },
+          error: (error) => {
+            console.error('Error al obtener detalles del personaje:', error);
+            this.personajeDetalle = null;
+          },
+        });
+      }
+    });
+  }
 }
