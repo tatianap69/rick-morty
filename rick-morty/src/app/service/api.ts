d39af68b2d1e6d776e0cdacac64cbcef76cbebca
tatianap69 <angieepaez69@gmail.com>
2025-06-07 21:11:38 -0500

    Agregar métodos generales

--- rick-morty/src/app/service/api.ts
+++ rick-morty/src/app/service/api.ts
@@ -1,20 +1,43 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Api {
   baseUrl: string = 'https://rickandmortyapi.com/api/character';
+  private personajesSubject = new BehaviorSubject<any[]>([]);
+  personajes$ = this.personajesSubject.asObservable();
 
   constructor(private http: HttpClient) {}
-
-  getPersonajes(limit: number = 20, page: number = 1): Observable<any> {
-    return this.http.get(`${this.baseUrl}?limit=${limit}&page=${page}`);
+  getPersonajes() {
+    return this.http.get<any>(`${this.baseUrl}`);
   }
 
   getDetallePersonaje(id: number): Observable<any> {
     return this.http.get(`${this.baseUrl}/${id}`);
+  }
+
+  getPersonajesPorNombre(nombre: string): Observable<any> {
+    return this.http.get(`${this.baseUrl}?name=${nombre}`);
+  }
+
+  resultadosBusqueda(nombre?: string) {
+    const request = nombre
+      ? this.getPersonajesPorNombre(nombre)
+      : this.getPersonajes();
+
+    request.subscribe({
+      next: (data) => this.personajesSubject.next(data.results),
+      error: () => this.personajesSubject.next([]),
+    });
+  }
+
+  cargarInicial() {
+    this.getPersonajes().subscribe({
+      next: (data) => this.personajesSubject.next(data.results),
+      error: () => this.personajesSubject.next([]),
+    });
   }
 }
